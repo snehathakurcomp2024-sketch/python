5cb7bcd1a64463126f9ced9680cec247434d776c
snehathakurcomp2024-sketch <sneha.thakur.comp.2024@vpkbiet.org>
2025-12-30 13:08:20 +0530

    Added 4x4 rectangle pattern

--- Untitled.py
+++ Untitled.py
@@ -1,20 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
-n = 5
-# Upper part
-for i in range(1, n + 1):
-    print(" " * (n - i) + "*" * (2 * i - 1))
-# Lower part
-for i in range(n - 1, 0, -1):
-    print(" " * (n - i) + "*" * (2 * i - 1))
-
-
-# In[ ]:
-
-
-
+for i in range(4):
+    print("* " * 4)
 
